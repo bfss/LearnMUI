d2be1d9762a72e4d6a6f22e3f08106f4481a9fbb
BeifangSusu <BeifangSusu@bfss.com>
2022-10-07 08:25:07 +0800

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,23 @@
-import { AppBar, Box, styled, Toolbar } from '@mui/material'
+import { AppBar, Box, styled, Toolbar, Typography } from '@mui/material'
+import { Pets } from '@mui/icons-material'
 import React from 'react'
 
 
 const SytledToolbar = styled(Toolbar)({
   // 对Toolbar应用样式
-  
+  display:"flex",
+  justifyContent:"space-between",
 })
 
 function Navbar() {
   return (
     <AppBar position='sticky'>
-      <Toolbar>
-        Navbar
-      </Toolbar>
+      <SytledToolbar>
+        <Typography variant='h6' sx={{dispaly:{xs:"none", sm:"block"}}}>
+            练习MUI
+        </Typography>
+        <Pets sx={{dispaly:{xs:"block", sm:"none"}}}/>
+      </SytledToolbar>
     </AppBar>
   )
 }
